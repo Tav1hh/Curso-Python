c3e9c795bc52d221469387c03c8035263aa80bfb
Otávio Santiago <otaviosatago@gmail.com>
2024-11-06 15:18:13 -0300

    #

---
+++ desafios/des006.py
@@ -0,0 +1,6 @@
+# Lé um número e mostra o seu dobro, triplo e raiz quadrada
+from math import sqrt
+n = int(input('Digite um número: '))
+print("O dobro é", n*2)
+print("O triplo é", n*3)
+print("A raiz quadrada é", sqrt(n))
