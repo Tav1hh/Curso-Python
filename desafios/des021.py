0d978940f6543c0c1351c56fc4957ab464bdfd17
Otávio Santiago <otaviosatago@gmail.com>
2024-11-07 15:21:12 -0300

    #

--- desafios/des021.py
+++ desafios/des021.py
@@ -1,6 +1,6 @@
 # Abre e reproduz um áudio MP3
-from pygame import mixer
-mixer.init()
-mixer.music.load('04 - Body And Soul.mp3')
-mixer.music.play()
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('04 - Body And Soul.mp3')
+pygame.mixer.music.play()
 x = input("Aperte Enter para finalizar o programa: ")
