1430c1f15d4298b05d0a17ba5f1de1b26540d0fe
Otávio Santiago <otaviosatago@gmail.com>
2024-11-11 16:02:56 -0300

    #

--- desafios/des032.py
+++ desafios/des032.py
@@ -1,13 +1,6 @@
 # Um programa que lé o ano e diz se ele e bissexto
 ano = str(input("Ano: "))
-if int(ano[-2:])%4 == 0:
-    if int(ano) >= 400:
-        if int(ano) % 400 == 0:
-            print("Ano Bissexto!")
-        else:
-            print("Ano Normal!")
-    else:
-        print("Ano Bissexto!")
-
+if int(ano[-2:])%4 == 0 or int(ano) >= 400 and int(ano) % 400 == 0:
+    print("Ano Bissexto!")
 else:
     print("Ano Normal!")
