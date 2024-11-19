57f6b1f446c2e2a09b6f90e08c267094b9370730
Otávio Santiago <otaviosatago@gmail.com>
2024-11-18 22:18:10 -0300

    #

--- desafios/des054.py
+++ desafios/des054.py
@@ -1,8 +1,11 @@
 # Crie um programa que leia o ano de nascimento de sete pessoas, no final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+from datetime import date
+anoAtual = date.today().year
 maior = 0
 menor = 0
 for c in range(1,7):
-    idade = int(input("Idade {}: ".format(c)))
+    anoNascimento = int(input("Ano Nascimento da {}º Pessoa: ".format(c)))
+    idade = anoAtual - anoNascimento
     if idade >= 21:
         maior = maior + 1
     else:
